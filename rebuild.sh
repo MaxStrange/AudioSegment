b0211e90fb6ebf4f40ea017efc944578fac2e9b7
MaxStrange <max.strange@synapse.com>
2017-08-25 20:23:21 -0700

    Added directions for tagging

--- rebuild.sh
+++ rebuild.sh
@@ -4,4 +4,6 @@ rm -rf audiosegment.egg-info/
 pipreqs --force .
 python3 setup.py bdist_wheel
 twine upload dist/*
-echo "Done. Please remember to make a release on github."
+echo "Done. Please remember to make a release on github via:"
+echo "git tag -a v<VERSION_NUMBER> -m \"<Message>\""
+echo "git push origin v<VERSION_NUMBER>"
