924836e257bb1261cb451835526d69ab4bdb9e55
Max Strange <maxfieldstrange@gmail.com>
2017-08-27 19:15:38 -0700

    Added FFT

--- audiosegment.py
+++ audiosegment.py
@@ -4,6 +4,7 @@ This module simply exposes a wrapper of a pydub.AudioSegment object.
 from __future__ import division
 
 import collections
+import numpy as np
 import pydub
 import os
 import subprocess
@@ -96,6 +97,13 @@ class AudioSegment:
 
             Yields tuples, one at a time, either ('v', Segment) or ('u', Segment).
             """
+            # TODO: Replace this with a Bayesian update algorithm.
+            #       We should mark each frame as voiced or unvoiced, then collect them
+            #       and update our belief about whether the sequence is voiced or not.
+            #       Once we get to a threshold probability that we are voiced, go back
+            #       and collect the last consecutive voiced frames and trigger, so that
+            #       until our probability drops below a different threshold (hysteresis),
+            #       we keep collecting frames as voiced regardless of what they are.
             construct_segment = lambda frames: AudioSegment(pydub.AudioSegment(data=b''.join([f.bytes for f in frames]),
                                                                           sample_width=self.sample_width,
                                                                           frame_rate=self.frame_rate,
@@ -105,14 +113,20 @@ class AudioSegment:
             triggered = False
             voiced_frames = []
             for frame in frames:
+                # Until we have collected 90% of num_padding_frames that we think are voiced,
+                # keep shoving frames into the ring buffer. Since it is a ring buffer, it
+                # drops old frames once we are at capacity.
                 if not triggered:
                     ring_buffer.append(frame)
                     num_voiced = len([f for f in ring_buffer if v.is_speech(f.bytes, self.frame_rate)])
                     if num_voiced > 0.9 * ring_buffer.maxlen:
+                        # We have collected enough voiced frames "in a row" to trigger
                         triggered = True
                         voiced_frames.extend(ring_buffer)
                         ring_buffer.clear()
                 else:
+                    # We are triggered, so collect each frame into voiced_frames until
+                    # enough of them "in a row" are unvoiced.
                     voiced_frames.append(frame)
                     ring_buffer.append(frame)
                     num_unvoiced = len([f for f in ring_buffer if not v.is_speech(f.bytes, self.frame_rate)])
@@ -154,6 +168,50 @@ class AudioSegment:
         self = other
         return self
 
+    def fft(self, start_s=None, duration_s=None, start_sample=None, num_samples=None):
+        """
+        Transforms the indicated slice of the AudioSegment into the frequency domain and returns the bins
+        and the values.
+
+        If neither start_s or start_sample is specified, the first sample of the slice will be the first sample
+        of the audio segment.
+
+        If neither duration_s or num_samples is specified, the slice will be from the specified start
+        to the end of the segment.
+
+        :param start_s: The start time in seconds. If this is specified, you cannot specify start_sample.
+        :param duration_s: The duration of the slice in seconds. If this is specified, you cannot specify num_samples.
+        :param start_sample: The zero-based index of the first sample to include in the slice.
+                             If this is specified, you cannot specify start_s.
+        :param num_samples: The number of samples to include in the slice. If this is specified, you cannot
+                            specify duration_s.
+        :returns: np.ndarray of frequencies, np.ndarray of amount of each frequency
+        :raises ValueError: If start_s and start_sample are both specified and/or if both duration_s and num_samples
+                            are specified.
+        """
+        if start_s is not None and start_sample is not None:
+            raise ValueError("Only one of start_s and start_sample can be specified.")
+        if duration_s is not None and num_samples is not None:
+            raise ValueError("Only one of duration_s and num_samples can be specified.")
+        if start_s is None and start_sample is None:
+            start_sample = 0
+        if duration_s is None and num_samples is None:
+            num_samples = len(self.get_array_of_samples()) - int(start_sample)
+
+        if duration_s is not None:
+            num_samples = int(round(duration_s * self.frame_rate))
+        if start_s is not None:
+            start_sample = int(round(start_s * self.frame_rate))
+
+        if start_sample + num_samples - 1 >= len(self.get_array_of_samples()):
+            raise ValueError("The combination of start and duration will run off the end of the AudioSegment object.")
+
+        end_sample = start_sample + num_samples  # end_sample is excluded
+        audioslice = np.array(self.get_array_of_samples()[start_sample:end_sample])
+        fft_result = np.real(np.fft.fft(audioslice))[range(int(round(num_samples/2)) + 1)]
+        bins = np.arange(0, int(round(num_samples/2)) + 1, 1.0) * (self.frame_rate / num_samples)
+        return bins, fft_result
+
     def generate_frames(self, frame_duration_ms, zero_pad=True):
         """
         Yields self's data in chunks of frame_duration_ms.
@@ -161,16 +219,19 @@ class AudioSegment:
         This function adapted from pywebrtc's example [https://github.com/wiseman/py-webrtcvad/blob/master/example.py].
 
         :param frame_duration_ms: The length of each frame in ms.
-        :param zero_pad: Whether or not to zero pad the end of the Segment object to get all the audio data out as frames. If not,
-                         there may be a part at the end of the Segment that is cut off (the part will be <= frame_duration_ms in length).
+        :param zero_pad: Whether or not to zero pad the end of the Segment object to get all
+                         the audio data out as frames. If not, there may be a part at the end
+                         of the Segment that is cut off (the part will be <= frame_duration_ms in length).
         :returns: A Frame object with properties 'bytes (the data)', 'timestamp (start time)', and 'duration'.
         """
         Frame = collections.namedtuple("Frame", "bytes timestamp duration")
 
-        bytes_per_frame = int(self.frame_rate * (frame_duration_ms / 1000) * self.sample_width)  # (samples/sec) * (seconds in a frame) * (bytes/sample)
+        # (samples/sec) * (seconds in a frame) * (bytes/sample)
+        bytes_per_frame = int(self.frame_rate * (frame_duration_ms / 1000) * self.sample_width)
         offset = 0  # where we are so far in self's data (in bytes)
         timestamp = 0.0  # where we are so far in self (in seconds)
-        frame_duration_s = (bytes_per_frame / self.frame_rate) / self.sample_width  # (bytes/frame) * (sample/bytes) * (sec/samples)
+        # (bytes/frame) * (sample/bytes) * (sec/samples)
+        frame_duration_s = (bytes_per_frame / self.frame_rate) / self.sample_width
         while offset + bytes_per_frame < len(self.raw_data):
             yield Frame(self.raw_data[offset:offset + bytes_per_frame], timestamp, frame_duration_s)
             timestamp += frame_duration_s
@@ -242,6 +303,59 @@ class AudioSegment:
 
         return wav_outs
 
+    def spectrogram(self, start_s=None, duration_s=None, start_sample=None, num_samples=None,
+                    window_length_s=None, window_length_samples=None, overlap=0.5):
+        """
+        Does a series of FFTs from start_s or start_sample for duration_s or num_samples.
+        Effectively, transforms a slice of the AudioSegment into the frequency domain across different
+        time bins.
+
+        :param start_s: The start time. Starts at the beginning if neither this nor start_sample is specified.
+        :param duration_s: The duration of the spectrogram in seconds. Goes to the end if neither this nor
+                           num_samples is specified.
+        :param start_sample: The index of the first sample to use. Starts at the beginning if neither this nor
+                             start_s is specified.
+        :param num_samples: The number of samples in the spectrogram. Goes to the end if neither this nor
+                            duration_s is specified.
+        :param window_length_s: The length of each FFT in seconds. If the total number of samples in the spectrogram
+                                is not a multiple of the window length in samples, the last window will be zero-padded.
+        :param window_length_samples: The length of each FFT in number of samples. If the total number of samples in the
+                                spectrogram is not a multiple of the window length in samples, the last window will
+                                be zero-padded.
+        :param overlap: The fraction of each window to overlap.
+        :returns: Three np.ndarrays: The frequency values in Hz (the y-axis in a spectrogram), the time values starting
+                  at start time and then increasing by duration_s each step (the x-axis in a spectrogram), and
+                  the dB of each time/frequency bin as a 2D array of shape [len(frequency values), len(duration)].
+        :raises ValueError: If start_s and start_sample are both specified, if duration_s and num_samples are both
+                            specified, if the first window's duration plus start time lead to running off the end
+                            of the AudioSegment, or if window_length_s and window_length_samples are either
+                            both specified or if they are both not specified.
+        """
+        if start_s is not None and start_sample is not None:
+            raise ValueError("Only one of start_s and start_sample may be specified.")
+        if duration_s is not None and num_samples is not None:
+            raise ValueError("Only one of duration_s and num_samples may be specified.")
+        if window_length_s is not None and window_length_samples is not None:
+            raise ValueError("Only one of window_length_s and window_length_samples may be specified.")
+        if window_length_samples is None and window_length_samples is None:
+            raise ValueError("You must specify a window length, either in window_length_s or in window_length_samples.")
+
+        if start_s is None and start_sample is None:
+            start_sample = 0
+        if duration_s is None and num_samples is None:
+            num_samples = len(self.get_array_of_samples()) - int(start_sample)
+
+        if duration_s is not None:
+            num_samples = int(round(duration_s * self.frame_rate))
+        if start_s is not None:
+            start_sample = int(round(start_s * self.frame_rate))
+
+        if start_sample + num_samples - 1 >= len(self.get_array_of_samples()):
+            raise ValueError("The combination of start and duration will run off the end of the AudioSegment object.")
+
+        # TODO
+
+
 def from_file(path):
     """
     Returns an AudioSegment object from the given file based on its file extension.
@@ -257,6 +371,8 @@ def from_file(path):
 
 # Tests
 if __name__ == "__main__":
+    import matplotlib.pyplot as plt
+
     if len(sys.argv) != 2:
         print("For testing this module, USAGE:", sys.argv[0], os.sep.join("path to wave file.wav".split(' ')))
         exit(1)
@@ -269,6 +385,22 @@ if __name__ == "__main__":
     print("Bits per sample:", seg.sample_width * 8)
     print("Sampling frequency:", seg.frame_rate)
     print("Length:", seg.duration_seconds, "seconds")
+
+    print("Doing FFT and plotting the histogram...")
+    print("  |-> Computing the FFT...")
+    hist_bins, hist_vals = seg.fft()
+    hist_vals = abs(hist_vals) / len(hist_vals)
+    print("  |-> Plotting...")
+    hist_vals_for_plot = 10 * np.log10(hist_vals + 1e-9)
+    plt.plot(hist_bins / 1000, hist_vals_for_plot, linewidth=0.02)
+    plt.xlabel("kHz")
+    plt.ylabel("dB")
+    plt.show()
+
+    print("Doing a spectrogram...")
+    print("  |-> Computing overlapping FFTs...")
+    hist_bins, times, amplitudes = seg.spectrogram(window_length_s=0.03, overlap=0.5)
+    print("  |-> Plotting...")
 
     print("Detecting voice...")
     seg = seg.resample(sample_rate_Hz=32000, sample_width=2, channels=1)
