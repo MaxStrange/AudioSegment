4d11cfc4ebfd665059258eb9af7cee09e48df2ad
Max Strange <max.strange@synapse.com>
2018-06-11 11:24:31 -0700

    Add rebuild script for Windows

---
+++ rebuild.ps1
@@ -0,0 +1,14 @@
+Remove-Item build -Recurse -Force
+Remove-Item dist -Recurse -Force
+Remove-Item audiosegment.egg-info -Recurse -Force
+
+pipreqs --force .
+
+python setup.py bdist_wheel
+
+twine upload dist/*
+
+Write-Host "Done. Please remember to make a release on github via:"
+Write-Host "git tag -a v<VERSION_NUMBER> -m <MSG>"
+Write-Host "git push origin v<VERSION_NUMBER>"
+
