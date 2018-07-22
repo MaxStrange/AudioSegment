bfad4a57b6f009ebeef5a738d7eeb7cc1b50d357
Max Strange <max.strange@synapse.com>
2018-07-22 12:47:26 -0700

    Update dice test so that it actually tests things

--- tests/trim.py
+++ tests/trim.py
@@ -1,14 +1,20 @@
 """
 Tests the ability to trim the audiosegment into slices.
 """
+import math
 import read_from_file
 import sys
 
 def test(seg):
-    print("Trimming to 30 ms slices...")
-    slices = seg.dice(seconds=0.03, zero_pad=True)
+    dice_len_s = 0.03
+    print("Trimming to {}s slices...".format(dice_len_s))
+    slices = seg.dice(seconds=dice_len_s, zero_pad=True)
     print("  |-> Got", len(slices), "slices.")
-    print("  |-> Durations in seconds of each slice:", [sl.duration_seconds for sl in slices])
+    print("  |-> Checking each one to make sure it is {}s in length...".format(dice_len_s))
+    for i, sl in enumerate(slices):
+        assert math.isclose(sl.duration_seconds, dice_len_s), "Slice {} out of {} is of duration {}s, but should be of duration {}s".format(
+            i, len(slices), sl.duration_seconds, dice_len_s
+        )
     return slices
 
 if __name__ == "__main__":
